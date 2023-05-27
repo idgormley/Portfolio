b92bccbf92f4d95db4f5e50a0a8365cfba38d542
idgormley <idgormley@gmail.com>
2023-05-27 15:35:06 -0500

    e

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if (bottom_of_window > bottom_of_element) {
-                $(this).fadeIn();
+                $(this).fadeIn("slow");
             }
         });
     });
