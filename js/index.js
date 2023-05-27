bf08115cd2f4e56b7884a6ed9e4e5c2613fcd65c
idgormley <idgormley@gmail.com>
2023-05-27 14:07:08 -0500

    d

--- js/index.js
+++ js/index.js
@@ -10,7 +10,22 @@ $(document).ready(function () {
             }
         });
     });
-        $('.fadeonload').each( function(i){
-            $(this).animate({'opacity':'1','margin-left':'0px'},1000);
-        }); 
+    $('.fadeonload').each(function (i) {
+        $(this).animate({ 'opacity': '1', 'margin-left': '0px' }, 1000);
+    });
+
+    // function menuButton{
+    //     console.log('clicked button');
+
+    //     let menubar = document.getElementById('menubar')
+
+    //     console.log(menubar)
+
+    //     // if (menubar.style.visibility === 'visible') {
+    //     //     menubar.style.visibility = 'hidden';
+    //     // } else if (menubar.style.visibility === 'hidden') {
+    //     //     menubar.style.visibility = 'visible';
+    //     // }
+    //     menubar.style.visibility='visible'? resume.style.visibility='hidden' : resume.style.visibility='visible';
+    // }
 });
