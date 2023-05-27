bc8b14bd6479c5ccf18b25a28653037559098e34
idgormley <idgormley@gmail.com>
2023-05-27 15:10:44 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,13 +2,8 @@ $(document).ready(function () {
     console.log("Your index.js file is loaded correctly")
 
     $(window).scroll(function () {
-        if ($(this).scrollBottom() > 0) {
-            $('.fade').fadeIn("slow");
-          } else {
-            $('.fade').fadeOut();
-          }
-
-        $('.fadein').each(function (i) {
+        
+        $('.fade').each(function (i) {
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if (bottom_of_window > bottom_of_element) {
